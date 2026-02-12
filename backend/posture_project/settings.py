34b15d8d06eb3df9eb548d713cd06fa241386066
Andreas Cormack <andreas@passthekeys.co.uk>
2026-02-12 20:19:22 +0000

    fix allowed hosts

--- backend/posture_project/settings.py
+++ backend/posture_project/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "True").lower() in ("true", "1")
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = [h.strip() for h in os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")]
 
 INSTALLED_APPS = [
     "daphne",
