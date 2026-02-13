fb64ca28fa828add643e90c77e3602dbe563a567
Andreas Cormack <andreas@passthekeys.co.uk>
2026-02-13 07:31:36 +0000

    added ability to add videos

--- backend/exercises/admin.py
+++ backend/exercises/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Exercise, ExerciseSet
+from .models import Exercise, ExerciseCategory, UserExerciseLog
 @admin.register(Exercise)
 class ExerciseAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'muscle_group')
-    search_fields = ('name', 'muscle_group')
+    list_display = ("name", "category", "target_issue", "difficulty", "is_premium")
+    list_filter = ("category", "target_issue", "difficulty", "is_premium")
+    search_fields = ("name", "description", "instructions")
